be5a716e52596b903799f3529035c721d001a7a1
Wei J. Zheng <wjzheng0191@gmail.com>
2022-01-14 16:34:15 -0500

    update demo: * allow reconnect to ws * consider paste and cut

--- demo/client/index.js
+++ demo/client/index.js
@@ -75,144 +75,161 @@ window.onload = function () {
         });
     };
 
-    var container = {};
+    function connectSocket() {
+        var container = {};
 
-    // Create WebSocket connection.
-    //var socket = new WebSocket("ws://142.11.215.231:42998");
-    var socket = new WebSocket("ws://localhost:42998");
-    // Connection opened
-    socket.addEventListener("open", function (event) {
-        $status.innerHTML = "ONLINE";
-        $field.disabled = false;
-        send({
-            type: "JOIN",
-            payload: {
-                name: $name.value
-            }
+        // Create WebSocket connection.
+        //var socket = new WebSocket("ws://142.11.215.231:42998");
+        var socket = new WebSocket("ws://localhost:42998");
+        // Connection opened
+        socket.addEventListener("open", function (event) {
+            $status.innerHTML = "ONLINE";
+            $field.disabled = false;
+            send({
+                type: "JOIN",
+                payload: {
+                    name: $name.value
+                }
+            });
         });
-    });
 
-    // Connection closed
-    socket.addEventListener("close", function (event) {
-        $status.innerHTML = "OFFLINE";
-        $field.disabled = true;
-    });
-    socket.addEventListener("error", function (event) {});
+        // Connection closed
+        socket.addEventListener("close", function (event) {
+            $status.innerHTML = "OFFLINE";
+            $field.disabled = true;
 
-    // Listen for messages
-    socket.addEventListener("message", function (event) {
-        var { type, payload } = JSON.parse(event.data);
+            setTimeout(() => {
+                //reconnect
+                connectSocket();
+            }, 5000);
+        });
+        socket.addEventListener("error", function (event) {});
 
-        //console.log(type, payload);
-        switch (type) {
-            case "JOIN":
-                $editors.innerHTML = payload.names;
-                setText(payload.shadow.value);
-                diffSync.initObject(container, payload.shadow.value);
-                $serverChance.value = payload.chance.server;
-                $clientChance.value = payload.chance.client;
-                break;
-            case "UPDATE_NAMES":
-                $editors.innerHTML = payload.names;
-                break;
-            case "SET_CHANCE":
-                $serverChance.value = payload.chance.server;
-                $clientChance.value = payload.chance.client;
-                break;
-            case "ACK":
-                console.log("RECEIVE ACK:", payload.n);
-                diffSync.onAck(container, payload);
-                break;
-            case "PATCH":
-                console.log("RECEIVE: ", payload);
+        // Listen for messages
+        socket.addEventListener("message", function (event) {
+            var { type, payload } = JSON.parse(event.data);
 
-                diffSync.onReceive({
-                    payload,
-                    container,
-                    onUpdateShadow(shadow, operations) {
-                        return diffSync.strPatch(shadow.value, operations);
-                    },
-                    onUpdateMain(patches, operations) {
-                        if (patches.length > 0) {
-                            let text = diffSync.strPatch($textfield.value, operations);
-                            //update main copy
-                            setText(text);
-                        }
-                        updateMNLabel();
-                    },
-                    afterUpdate(m) {
-                        console.log("SEND ACK", m);
-                        send({
-                            type: "ACK",
-                            payload: {
-                                m
+            //console.log(type, payload);
+            switch (type) {
+                case "JOIN":
+                    $editors.innerHTML = payload.names;
+                    setText(payload.shadow.value);
+                    diffSync.initObject(container, payload.shadow.value);
+                    $serverChance.value = payload.chance.server;
+                    $clientChance.value = payload.chance.client;
+                    break;
+                case "UPDATE_NAMES":
+                    $editors.innerHTML = payload.names;
+                    break;
+                case "SET_CHANCE":
+                    $serverChance.value = payload.chance.server;
+                    $clientChance.value = payload.chance.client;
+                    break;
+                case "ACK":
+                    console.log("RECEIVE ACK:", payload.n);
+                    diffSync.onAck(container, payload);
+                    break;
+                case "PATCH":
+                    console.log("RECEIVE: ", payload);
+
+                    diffSync.onReceive({
+                        payload,
+                        container,
+                        onUpdateShadow(shadow, operations) {
+                            return diffSync.strPatch(shadow.value, operations);
+                        },
+                        onUpdateMain(patches, operations) {
+                            if (patches.length > 0) {
+                                let text = diffSync.strPatch($textfield.value, operations);
+                                //update main copy
+                                setText(text);
                             }
-                        });
-                    }
-                });
+                            updateMNLabel();
+                        },
+                        afterUpdate(m) {
+                            console.log("SEND ACK", m);
+                            send({
+                                type: "ACK",
+                                payload: {
+                                    m
+                                }
+                            });
+                        }
+                    });
 
-                break;
-        }
-    });
-
-    function updateMNLabel() {
-        const shadow = container.shadow;
-        $mnLabel.innerHTML = `N:${shadow.n} M:${shadow.m}`;
-    }
-
-    function send(data) {
-        socket.send(JSON.stringify(data));
-    }
-
-    function setText(val) {
-        var sel = getInputSelection($textfield);
-        var textVal = $textfield.value;
-        var isFrontChange = val.substring(0, sel.start) !== textVal.substring(0, sel.start);
-        if (isFrontChange) {
-            var charDiff = textVal.length - val.length;
-            sel.start -= charDiff;
-            sel.end -= charDiff;
-        }
-        $textfield.value = val;
-        setInputSelection($textfield, sel.start, sel.end);
-    }
-
-    //DIFFER SYNC
-
-    var timeout = null;
-    $textfield.onkeyup = function () {
-        clearTimeout(timeout);
-
-        var delay = parseInt($timeoutDelay.value);
-
-        if (delay > 0) {
-            $timeoutIndicator.innerHTML = "Processing...";
-            timeout = setTimeout(() => {
-                $timeoutIndicator.innerHTML = "Sent!";
-                sendPatch(this.value);
-            }, delay);
-        } else {
-            sendPatch(this.value);
-        }
-    };
-
-    function sendPatch(text) {
-        diffSync.onSend({
-            container,
-            mainText: text,
-            whenSend(m, edits) {
-                updateMNLabel();
-                const payload = {
-                    m,
-                    edits
-                };
-                console.log("SEND: ", payload);
-
-                send({
-                    type: "PATCH",
-                    payload
-                });
+                    break;
             }
         });
+
+        function updateMNLabel() {
+            const shadow = container.shadow;
+            $mnLabel.innerHTML = `N:${shadow.n} M:${shadow.m}`;
+        }
+
+        function send(data) {
+            socket.send(JSON.stringify(data));
+        }
+
+        function setText(val) {
+            var sel = getInputSelection($textfield);
+            var textVal = $textfield.value;
+            var isFrontChange = val.substring(0, sel.start) !== textVal.substring(0, sel.start);
+            if (isFrontChange) {
+                var charDiff = textVal.length - val.length;
+                sel.start -= charDiff;
+                sel.end -= charDiff;
+            }
+            $textfield.value = val;
+            setInputSelection($textfield, sel.start, sel.end);
+        }
+
+        //DIFFER SYNC
+        var timeout = null;
+        function processSend(value) {
+            clearTimeout(timeout);
+
+            var delay = parseInt($timeoutDelay.value);
+
+            if (delay > 0) {
+                $timeoutIndicator.innerHTML = "Processing...";
+                timeout = setTimeout(() => {
+                    $timeoutIndicator.innerHTML = "Sent!";
+                    sendPatch(value);
+                }, delay);
+            } else {
+                sendPatch(value);
+            }
+        }
+
+        $textfield.onpaste = $textfield.oncut = function (e) {
+            processSend(this.value);
+        };
+
+        $textfield.onkeyup = function (e) {
+            if (e.type === "keyup") {
+                processSend(this.value);
+            }
+        };
+
+        function sendPatch(text) {
+            diffSync.onSend({
+                container,
+                mainText: text,
+                whenSend(m, edits) {
+                    updateMNLabel();
+                    const payload = {
+                        m,
+                        edits
+                    };
+                    console.log("SEND: ", payload);
+
+                    send({
+                        type: "PATCH",
+                        payload
+                    });
+                }
+            });
+        }
     }
+    connectSocket();
 };
